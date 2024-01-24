feedc685d6e38920a385b5cef2e050dc03776d38
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2024-01-24 22:24:33 +0900

    d

---
+++ src/MainApp.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import FallbackSpinner from './components/FallbackSpinner';
+import NavBarWithRouter from './components/NavBar';
+import Home from './components/Home';
+
+function MainApp() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/profile/routes.json', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => err);
+  }, []);
+
+  return (
+    <div className="MainApp">
+      <NavBarWithRouter />
+      <main className="main">
+        <Switch>
+          <Suspense fallback={<FallbackSpinner />}>
+            <Route exact path="/" component={Home} />
+            {data
+              && data.sections.map((route) => {
+                const SectionComponent = React.lazy(() => import('./components/' + route.component));
+                return (
+                  <Route
+                    key={route.headerTitle}
+                    path={route.path}
+                    component={() => (
+                      <SectionComponent header={route.headerTitle} />
+                    )}
+                  />
+                );
+              })}
+          </Suspense>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default MainApp;
